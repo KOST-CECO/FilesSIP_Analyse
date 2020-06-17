b7c6e014d0ce0a57c6f497b56d67a09e10a9fc3c
mkaiser56 <m.kaiser@access.ch>
2020-06-17 09:46:38 +0200

    read prog arguments

--- FilesSIP_Analyse.go
+++ FilesSIP_Analyse.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+// read prog arguments, test directory and start directory walk
 func main() {
-	fmt.Printf("hello, world\n")
+
+	// read initial parameter from command line
+	if len(os.Args) != 2 {
+		fmt.Println("usage: " + os.Args[0] + " folder")
+		os.Exit(-1)
+	}
+
+	// check for valid folder
+	fo, foerr := os.Stat(os.Args[1])
+	if foerr != nil {
+		log.Fatal(os.Args[1] + " is not a valid folder name")
+	}
+	if !fo.IsDir() {
+		log.Fatal(os.Args[1] + " is not a folder")
+	}
+
 }
